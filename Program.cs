8df36901a2de6188de92c3d25e261eec19372c3f
Докучаев Артем Игоревич <A.Dokuchaev@cft.ru>
2022-10-13 15:19:11 +0700

    загрузка строк пользователем

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Количество строк, которые хотите ввести: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int size = 0;
+string[] massiv = new string[n];
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите строку №{0}: ", i + 1);
+    massiv[i] = Console.ReadLine();
+    if (massiv[i].Length == 3) size++;
+}
+
+string[] createMass = NewMassCreate(massiv, size);
+NewMasPrint(massiv, createMass);
